02d67ca25436a1bbdbcb9eb84d2aa7fe1395d2fe
paxman234 <sav-fernandes@outlook.com>
2024-08-13 12:15:39 +0100

    pAequor - factory objects structure.

--- main.js
+++ main.js
@@ -14,50 +14,72 @@ const mockUpStrand = () => {
 };
 
 const pAequorFactory = (specimenNum, dna) => {
+  return {
+    specimenNum: specimenNum,
+    dna: dna,
+    mutate() {
+      const indexDnaBase = Math.floor(Math.random() * 14);
+      let randomDnaBase = this.dna[indexDnaBase];
+      let newDnaBase = returnRandBase();
+      while(newDnaBase === randomDnaBase) {
+        newDnaBase = returnRandBase();
+      }
+      const mutatedDna = this.dna.splice(indexDnaBase, 1, newDnaBase);
+      mutatedDna;
+    },
   
-
-  function mutate() {
-    const indexDnaBase = Math.floor(Math.random() * 14)
-    let randomDnaBase = dna[indexDnaBase];
-    let newDnaBase = returnRandBase();
-    while(newDnaBase === randomDnaBase) {
-      newDnaBase = returnRandBase();
-    }
-    const mutatedDna = dna.splice(indexDnaBase, 1, newDnaBase);
-    return mutatedDna;
-  };
-
-  function compareDna(pAequor) {
-    let percentage = 0;
-    let match_count = 0;
-    let difference_count = 0;
-    for (let i = 0; i < 15; i++) {
-      for(let j = 0; j < 15; j++) {
-        if(dna[i] === pAequor.dna[j]) {
-          match_count++;
-        }
-        else {
-          difference_count++;
+    compareDna (pAequor) {
+      let percentage = 0;
+      let match_count = 0;
+      let difference_count = 0;
+      for (let i = 0; i < 15; i++) {
+        for(let j = 0; j < 15; j++) {
+          if(this.dna[i] === pAequor.dna[j]) {
+            match_count++;
+          }
+          else {
+            difference_count++;
+          }
         }
       }
-    }
-    percentage=100/15 * match_count;
-    return `specimen #${specimenNum} and specimen #2 have ${percentage}% DNA in common`;
-  }
-  function willLikelySurvive() {
-    let cOrGBases_count = 0;
-    const survivalNumber = 9;
-    for (const dnaBase of dna) {
-      if(dnaBase === 'C' || dnaBase === 'G') {
-        cOrGBases_count++;
+      percentage=100/15 * match_count;
+      return `specimen #${specimenNum} and specimen #2 have ${percentage}% DNA in common`;
+    },
+  
+    willLikelySurvive() {
+      let cOrGBases_count = 0;
+      const survivalNumber = 9;
+      for (const dnaBase of dna) {
+        if(dnaBase === 'C' || dnaBase === 'G') {
+          cOrGBases_count++;
+        }
       }
+      return cOrGBases_count >= survivalNumber ? true : false;
     }
-    return cOrGBases_count >= survivalNumber ? true : false;
   }
+
 } 
 
+let otherSpecimen = [];
+let survivalSpecimens = [];
+let specNum = 1;
+// const currentStrand = mockUpStrand;
+while(survivalSpecimens.length < 30) {
+  const currentStrand = mockUpStrand;
+  const currentSpecimen = pAequorFactory(specNum, currentStrand);
+  const survivability = currentSpecimen.willLikelySurvive();
+  if(survivability) {
+    survivalSpecimens.push(currentSpecimen);
+  }
+  else {
+    otherSpecimen.push(currentSpecimen);
+  }
+  specNum ++;
 
-
+}
+console.log(specNum);
+console.log(survivalSpecimens.length);
+console.log(otherSpecimen.lengthecNum);
 
 
 
