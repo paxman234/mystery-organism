cc6cff132fa60f7a601d00cea29ddbab4c6b4ad7
paxman234 <sav-fernandes@outlook.com>
2024-08-16 12:17:48 +0100

    solution changes cheatsheet and remove comments

--- main.js
+++ main.js
@@ -16,64 +16,27 @@ const mockUpStrand = () => {
 const pAequorFactory = (specimenNum, dna) => {
   return {
     //specimen properties
-    _specimenNum: specimenNum,
-    _dna: dna,
-    get specimenNum() {
-      if(typeof this._specimenNum === 'number') {
-        return this._specimenNum;
-      }
-      else {
-        return 'spec type error.';
-      }
-    },
-    set specimenNum(specimenNum) {
-      if(typeof specimenNum === 'number') {
-        return this._specimenNum = specimenNum;
-      }
-      else {
-        return 'Pass a unique number above 0';
-      }
-    },
-    //dna properties
-    get dna(){
-      if(Array.isArray(this._dna)) {
-        return this._dna;
-      }
-      else {
-        return `dna for specimen #${this._specimenNum} not retrievable.`
-      }
-    },
-    set dna(dna) {
-      if(Array.isArray(dna)) {
-        return this._dna = dna;
-      }
-      else {
-        return 'Make sure DNA data is of correct type.';
-      }
-    },
+    specimenNum,
+    dna,
     //specimen methods.
     mutate() {
       const indexDnaBase = Math.floor(Math.random() * 14);
-      let randomDnaBase = this._dna[indexDnaBase];
+      let randomDnaBase = this.dna[indexDnaBase];
       let newDnaBase = returnRandBase();
       while(newDnaBase === randomDnaBase) {
         newDnaBase = returnRandBase();
       }
-      this._dna.splice(indexDnaBase, 1, newDnaBase);
-      return this._dna;
+      this.dna.splice(indexDnaBase, 1, newDnaBase);
+      return this.dna;
     },
   
     compareDna(pAequor) {
       let percentage = 0;
       let match_count = 0;
-      // let difference_count = 0;
       for (let i = 0; i < 15; i++) {
         if(this.dna[i] === pAequor.dna[i]) {
           match_count++;
         }
-        // else {
-        //   difference_count++;
-        // }
       }
       percentage=100/15 * match_count;
       return `specimen #${this.specimenNum} and specimen #${pAequor.specimenNum} have ${Math.floor(percentage)}% DNA in common`;
@@ -88,6 +51,10 @@ const pAequorFactory = (specimenNum, dna) => {
         }
       }
       return cOrGBases_count >= survivalNumber ? true : false;
+    },
+
+    complementStrand() {
+
     }
   }
 
@@ -101,8 +68,7 @@ let currentStrand = [];
 let specNum = 1;
 // let mutatedSpecimen = [];
 while(survivalSpecimens.length < 30) {
-  currentStrand = mockUpStrand();
-  const currentSpecimen = pAequorFactory(specNum, currentStrand);
+  const currentSpecimen = pAequorFactory(specNum, mockUpStrand());
   const survivability = currentSpecimen.willLikelySurvive();
   if(survivability) {
     survivalSpecimens.push(currentSpecimen);
